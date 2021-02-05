2f21ccfb6c7246e7302e7278e0087acfa0762b19
Aske Mottelson <aske@mottelson.dk>
2021-02-05 16:14:54 +0100

    done

---
+++ Assets/lib/UnityQuestionnaire/questionnaire/Assets/Scripts/UserFollower.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserFollower : MonoBehaviour {
+    public GameObject User;
+
+    void Start() {
+        
+    }
+
+    void Update() {
+        var newPos = User.transform.position + User.transform.forward * 0.4f;
+        transform.position = Vector3.Lerp(transform.position, newPos, 0.05f);
+
+        var toUser = User.transform.position - transform.position;
+        //toUser = Vector3.ProjectOnPlane(toUser, Vector3.up).normalized;
+        transform.rotation = Quaternion.LookRotation(toUser);       
+    }
+}

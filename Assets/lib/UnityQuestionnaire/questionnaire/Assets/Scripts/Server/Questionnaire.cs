e4354fb01c202968e0a76d6f433c66530205b6dc
Aske Mottelson <aske@mottelson.dk>
2021-02-05 16:19:47 +0100

    rm lib

--- Assets/lib/UnityQuestionnaire/questionnaire/Assets/Scripts/Server/Questionnaire.cs
+++
@@ -1,8 +0,0 @@
-[System.Serializable]
-public class Questionnaire
-{
-    //these variables are case sensitive and must match the strings "firstName" and "lastName" in the JSON.
-    public string name;
-    public Question[] questions;
-    public int[] exclude_rounds;
-}
--- Assets/lib/UnityQuestionnaire/questionnaire/Assets/Scripts/Server/Questionnaires.cs
+++
@@ -1,5 +0,0 @@
-[System.Serializable]
-public class Questionnaires
-{
-    public Questionnaire[] questionnaires;
-}
